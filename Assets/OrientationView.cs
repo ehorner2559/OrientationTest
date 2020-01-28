a780abb9b834394c795510dd3b7b1d41aac6e717
Vygar <erik.horner@comcast.net>
2020-01-28 09:25:08 -0500

    OrientationTest

---
+++ Assets/OrientationView.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using TMPro;
+
+public class OrientationView : MonoBehaviour
+{
+    [SerializeField]
+    private TextMeshProUGUI textField = null;
+
+    private void Update()
+    {
+        textField.text = Input.deviceOrientation.ToString();
+    }
+}
